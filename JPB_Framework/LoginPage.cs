52491482db6e072f2e20432bb5c5e6cbb78fc512
pmavrogiannis <p.mavrogiannis@CSC-08.sieben.gr>
2016-02-16 19:29:59 +0200

    Initial Commit

---
+++ JPB_Framework/LoginPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+
+namespace JPB_Framework
+{
+    public class LoginPage
+    {
+
+        public static void GoTo()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://jpbstaging.azurewebsites.net");
+        }
+
+        public static LoginCommand LoginAs(string username)
+        {
+            return new LoginCommand(username);
+        }
+
+        public static bool IsAt()
+        {
+            
+        }
+    }
+
+    public class LoginCommand
+    {
+        private string username;
+        private string password;
+
+        public LoginCommand(string username)
+        {
+            this.username = username;
+        }
+
+        public LoginCommand WithPassword(string password)
+        {
+            this.password = password;
+            return this;
+        }
+
+        public void Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JPB_Tests/LoginTests.cs
@@ -0,0 +1,19 @@
+﻿using JPB_Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JPB_Tests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void Can_Go_Login_Page()
+        {
+            LoginPage.GoTo();
+            Assert.IsTrue(LoginPage.IsAt, "Is not at Login Page");
+
+            LoginPage.LoginAs("panagiotis@panagof1.com").WithPassword("6AB10F93").Login();
+            Assert.IsTrue(ContactsPage.IsAt, "Failed to login");
+        }
+    }
+}
