db77b770deb3726507f44491f31fc563eac9750c
Panagiotis <p.mavrogiannis@outlook.com>
2016-03-24 17:52:28 +0200

    no message

--- JPB_Framework/Pages/Contacts/ImportContactsWindow.cs
+++ JPB_Framework/Pages/Contacts/ImportContactsWindow.cs
@@ -1,18 +1,39 @@
 ﻿using JPB_Framework.UI_Utilities;
+using OpenQA.Selenium;
 
 namespace JPB_Framework.Pages.Contacts
 {
+    /// <summary>
+    /// Workflow class that handles the actions regarding the import contacts dialog window
+    /// </summary>
     public class ImportContactsWindow
     {
+        /// <summary>
+        /// Opens the import contacts dialog window
+        /// </summary>
         public static void GoTo()
         {
             Commands.ClickImport();
         }
 
+        /// <summary>
+        /// Defines the path that contains the files to be imported.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
         public static ImportFileCommand FromPath(string filePath)
         {
             GoTo();
             return new ImportFileCommand(filePath);
+        }
+
+        /// <summary>
+        /// Commands the browser to download a contact template file from within the import contacts dialog window
+        /// </summary>
+        public static void DownloadTemplateFile()
+        {
+            GoTo();
+            Driver.Instance.FindElement(By.CssSelector("a[ng-href='import/Contacts.xls']")).Click();
         }
     }
 
---
+++ JPB_Tests/ContactsTests/ImportContactsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JPB_Framework;
+using JPB_Framework.Pages.Contacts;
+using JPB_Tests.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JPB_Tests.ContactsTests
+{
+    [TestClass]
+    public class ImportContactsTests : JpbBaseTest
+    {
+
+        // 1. Check that contacts template is successfully downloaded 
+        [TestMethod]
+        public void Contacts_Template_Is_Downloaded_Successfully()
+        {
+            ImportContactsWindow.DownloadTemplateFile();
+
+        }
+
+        // 2. Import contacts - all contact fields are filled 
+
+        // 3. Import contacts - only mandatory fields are filled
+
+        // 4. Import contacts - only mandatory fields are left unfilled
+
+        // 5. Import contacts - only mandatory fields are filled. Organization field takes existent value
+
+        // 6. Import contacts - only mandatory fields are filled. Organization field takes nonexistent value
+
+        // 7. Import contacts - Fields are filled with nonsense values
+
+        // 8. Import contacts - Fields are filled with values so that it cause field character overflow
+
+        // 9. Import contacts - Test the max imported contact threshold
+
+        // 10. Import contacts - Birthdate field contains invalid for date
+
+        // 11. Import contacts - Template contains less columns than the original template
+
+        // 12. Import contacts - Template contains more columns that the original template
+
+        // 13. Import contacts - Template contains null rows between normal contact rows
+
+        // 14. Immport contacts - Template does not contain the mandatory field column
+
+        // 15. Import ontacts - Template contains duplicate contacts
+
+        // 16. Import contacts - Template contains columns in different order than that of the original template
+
+    }
+}
